7d18946882403df86e4e87a211e655c3008e4681
Shivam Ahlawat <747797@student.tor.loc>
2019-06-19 16:36:03 -0400

    second commit

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("hello evryone");
+// factory function
+function createCircle(radius){
+    return {
+       radius,
+       draw: function(){
+           console.log('draw');
+       }
+   };   
+   }
+   const circle = createCircle(1);
+   circle.draw();
+   
+   // Constructor Function
+   
+   function Circle(radius){
+       console.log('this', this);
+       this.radius = radius;
+       this.draw = function(){
+           console.log("draw");
+       }
+   }
+   
+   const another = new Circle(1);
